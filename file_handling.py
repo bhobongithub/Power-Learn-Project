f19d279555d74096a2fc936c9069cdfa4bf2e516
bhobongithub <156057547+bhobongithub@users.noreply.github.com>
2025-08-11 17:25:53 +0800

    Create file_handling.py

---
+++ file_handling.py
@@ -0,0 +1,23 @@
+file_name = input("Please input the name of the txt file you wish to read: ")
+try:
+    with open(file_name, "r") as file:
+        data = file.read()
+except FileNotFoundError:
+    print("File not found. Please check the filename.")
+    file_name = input("Please input the name of the txt file you wish to read: ")
+    try:
+       with open(file_name, "r") as file:
+           data = file.read() 
+    except FileNotFoundError:
+        print("File still not found. Pleaase start again.")
+
+words = data.split()
+word_count = len (words)
+upper_case = data.upper()
+
+with open("output.txt", "w") as file2:
+    file2.write(upper_case)
+    file2.write(str(word_count))
+    print ("The output file has been successfully created and it contains ", word_count, "words")
+
+
